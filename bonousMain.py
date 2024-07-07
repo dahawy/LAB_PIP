867445e0dd887acb23ff7f9ca72d0edb32e60765
D. F <ifonei@hotmail.com>
2024-07-07 23:36:04 +0300

    addig .gitignore file to the repo

--- bonousMain.py
+++ bonousMain.py
@@ -1,7 +1,7 @@
 from art import *
 from colorama import Fore,Back,Style
 
-tprint("GOOd MORNING",font="rd",chr_ignore=True)
+tprint("GOOD MORNING",font="random",chr_ignore=True)
 tprint("EXCELLENT",font= "bulbhead",chr_ignore=True)
 
 print(Fore.BLUE + "TEXT IN TERMINAL CHANGED TO BLUE")
