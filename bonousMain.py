67c588e6769ee3ba2bd9f731b2cc5e169cde21e7
D. F <ifonei@hotmail.com>
2024-07-08 00:04:14 +0300

    untracking venv

--- bonousMain.py
+++ bonousMain.py
@@ -3,5 +3,4 @@ from colorama import Fore,Back,Style
 
 tprint("GOOD MORNING",font="random",chr_ignore=True)
 tprint("EXCELLENT",font= "bulbhead",chr_ignore=True)
-
 print(Fore.BLUE + "TEXT IN TERMINAL CHANGED TO BLUE")
